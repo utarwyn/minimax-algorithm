3333c2417c355351ee8ee10db94897ad02a01c25
utarwyn <maxime.malgorn@laposte.net>
2021-06-04 01:12:09 +0200

    feat: add tests for Minimax algorithm

--- MinimaxTests/MinimaxTest.cs
+++ MinimaxTests/MinimaxTest.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Threading;
-using MinimaxAlgorithm;
+﻿using MinimaxAlgorithm;
 using NUnit.Framework;
 
 namespace MinimaxTests
@@ -17,26 +15,56 @@ namespace MinimaxTests
         }
 
         [Test]
-        public void MaxAlphaBeta()
+        public void BlockOtherToWin()
+        {
+            _game.SetMove(0, Player.First);
+            _game.SetMove(3, Player.First);
+            _game.SetMove(1, Player.Second);
+            _game.SetMove(4, Player.Second);
+
+            var (score, index) = Minimax.Instance.MinAlphaBeta(_game);
+            _game.SetMove(2, Player.First);
+            var (_, max) = Minimax.Instance.MaxAlphaBeta(_game);
+
+            Assert.AreEqual(-1, score);
+            Assert.AreEqual(6, index);
+            Assert.AreEqual(7, max);
+        }
+
+        [Test]
+        public void MaximizeChancesToWin()
+        {
+            _game.SetMove(0, Player.First);
+            _game.SetMove(7, Player.First);
+            _game.SetMove(1, Player.Second);
+
+            var (score, index) = Minimax.Instance.MaxAlphaBeta(_game);
+            Assert.AreEqual(0, score);
+            Assert.AreEqual(6, index);
+        }
+
+        [Test]
+        public void TieGame()
         {
             while (_game.GetWinner() == null)
             {
+                int index;
                 if (_game.CurrentPlayer == Player.First)
                 {
-                    var (_, index) = Minimax.Instance.MinAlphaBeta(_game);
-                    _game.SetMove(index, _game.CurrentPlayer);
+                    (_, index) = Minimax.Instance.MinAlphaBeta(_game);
                 }
                 else
                 {
-                    var (_, index) = Minimax.Instance.MaxAlphaBeta(_game);
-                    _game.SetMove(index, _game.CurrentPlayer);
+                    (_, index) = Minimax.Instance.MaxAlphaBeta(_game);
                 }
 
-                Console.WriteLine(_game);
+                _game.SetMove(index, _game.CurrentPlayer);
                 _game.GoNextPlayer();
             }
-            
-            Console.WriteLine($"And the winner is {_game.GetWinner()?.ToString()}!");
+
+            // Players use the same algorithm, so it must be a tie!
+            Assert.AreEqual(Player.Empty, _game.GetWinner());
+            Assert.AreEqual("| X | X | O |\n| O | O | X |\n| X | O | X |\n", _game.ToString());
         }
     }
 }
